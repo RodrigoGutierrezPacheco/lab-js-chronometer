7229c89d7e43636db38a5f3c4d51fcdfd2e0ccc1
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-08-16 18:28:42 -0500

    Solved-lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+   this.currentTime = 0;
+	 this.intervalId = null;
   }
 
-  start(printTimeCallback) {
-    // ... your code goes here
+  start(printTimeCallback){
+    this.intervalId = setInterval(()=>{
+			this.currentTime+=1;
+			if(printTimeCallback){
+				printTimeCallback();
+			}
+		},1000);
   }
+	
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+  return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+   if (value < 10){
+		return "0" + value;
+	 }
+	 return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+     clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+		return this.currentTime=0;
   }
 
   split() {
     // ... your code goes here
-  }
+    const min = this.getMinutes();
+		const sec = this.getSeconds();
+		this.computeTwoDigitNumber(min),
+		this.computeTwoDigitNumber(sec)
+
+		let minutosFormateados = this.computeTwoDigitNumber(min);
+		let segundosFormateados = this.computeTwoDigitNumber(sec);
+
+		return minutosFormateados+":"+segundosFormateados;
+		
+};
+
 }
